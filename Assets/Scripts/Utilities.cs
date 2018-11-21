ab11255703124a315aa641b0ef00ec647f76351c
pingumen96 <sinhue.rossi@gmail.com>
2018-11-21 15:59:52 +0100

    Init

---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities {
+
+    public static class Math {
+        static float randomBinomial() {
+            return Random.Range(-1f, 1f) - Random.Range(-1f, 1f);
+        }
+
+    }
+}
---
+++ Assets/Scripts/WanderingMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utilities;
+
+public class WanderingMovement : AMovement {
+    [Serializable] public Vector3 wanderOffset;
+    [Serializable] public float wanderRadius;
+    [Serializable] public float wanderRate;
+
+    protected override void getSteering() {
+        float wanderOrientation 
+    }
+}
